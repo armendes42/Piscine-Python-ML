8f8206839951eaa8be85cc7b7e29ef5b8dc925c2
armendes <arthursednem@gmail.com>
2023-06-13 18:41:07 +0200

    07 start of 08

---
+++ PythonModule00/ex07/filterwords.py
@@ -0,0 +1,13 @@
+import sys
+import string
+
+if len(sys.argv) != 3:
+    print("Error: Wrong number of arguments")
+elif not isinstance(sys.argv[1], str) or not sys.argv[2].isdigit():
+    print("Error: One or both of the type of arguments is wrong")
+else:
+    new_str = sys.argv[1].translate(str.maketrans('', '', string.punctuation))
+    word_list = list(new_str.split(" "))
+    new_word_list = [i for i in word_list if len(i) > int(sys.argv[2])]
+    new_word_list = [i for i in new_word_list ]
+    print(new_word_list)
